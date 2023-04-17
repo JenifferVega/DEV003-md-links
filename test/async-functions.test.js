f7148f817af4684273d1e33817aa38d9ac4b767f
JenifferVega <kotomivega@gmail.com>
2023-04-16 20:28:59 -0500

    test de carpeta function al 87%

--- test/async-functions.test.js
+++ test/async-functions.test.js
@@ -1,10 +1,7 @@
-
-
+// pruebas de forma asincrona con la carpeta functions
 const {
     absolutePathRoute,
     validExtension,
-    readFilePromise,
-    validateLink,
   } = require("../function");
 
 
@@ -31,52 +28,4 @@ describe('validExtension', () => {
     expect(result).toBeFalsy();
     done();
   });
-});/*
-
-describe('readFilePromise', () => {
-  test('should return the content of a file as a string', (done) => {
-    readFilePromise('./files/demo.md', (err, result) => {
-      expect(err).toBeNull();
-      expect(result.toString()).toBe('# Test\n');
-      done();
-    });
-  },10000);
-
-  test('should return an error for a non-existing file', (done) => {
-    readFilePromise('./testFiles/non-existing.md', (err, result) => {
-      expect(err).toBeTruthy();
-      expect(result).toBeUndefined();
-      done();
-    });
-  },10000);
 });
-
-describe('validateLink', () => {
-  test('should validate a link with a 200 status code', (done) => {
-    const link = { href: 'https://jsonplaceholder.typicode.com/posts/1' };
-    validateLink(link, (result) => {
-      expect(result.status).toBe(200);
-      expect(result.message).toBe('OK');
-      done();
-    });
-  },10000);
-
-  test('should validate a link with a 404 status code', (done) => {
-    const link = { href: 'https://jsonplaceholder.typicode.com/non-existing' };
-    validateLink(link, (result) => {
-      expect(result.status).toBe(404);
-      expect(result.message).toBe('Not Found');
-      done();
-    });
-  },10000);
-
-  test('should handle an error when validating a link', (done) => {
-    const link = { href: 'https://non-existing-website.com' };
-    validateLink(link, (result) => {
-      expect(result.status).toBe('N/A');
-      expect(result.message).toBe('ok');
-      done();
-    });
-  },10000);
-});
-*/
